7cb1f56af2b0f4c66e560c9becdc98e061d1c16e
Yncy0 <paalayancy@gmail.com>
2025-05-23 08:14:36 +0800

    update: code for controlling directory visibility

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,15 +8,19 @@ return {
   lazy = false,
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end,
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          never_show = { ".git"}
+        }
       }
     }
+  end,
+  opts = {
+
   }
 }
 
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,20 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files'})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+          ".git/",
+          ".nuxt",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true
+        }
+      }
+    })
   end
 }
