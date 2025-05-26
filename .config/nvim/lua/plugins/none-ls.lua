2239a05c7e07ba8a89d23135b84ce421c0d51d05
Yncy0 <paalayancy@gmail.com>
2025-05-26 14:45:48 +0800

    added: eslint_d for eslint validation -> none-ls.lua

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -7,6 +10,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
+        require("none-ls.diagnostics.eslint_d")
 			},
 		})
 
