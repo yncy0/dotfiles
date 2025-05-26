811a99a66d69b162bcb6b75fc2e9f43bb3eaa342
Yncy0 <paalayancy@gmail.com>
2025-05-26 13:27:10 +0800

    added: cmp.lua -> nvim-cmp, luasnip, cmp-nvim-lsp, added: cmp configuration at lsp-config.lua using cmp-nvim-lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -5,8 +5,9 @@ return {
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
-    },  
-    config = function()
+    },    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       require("mason").setup({
         ui = {
           icons = {
@@ -27,28 +28,42 @@ return {
         automatic_installation = true
       })
 
-        vim.lsp.enable({
-          'lua_ls',
-          'eslint',
-          'gdscript',
-          'prettier',
-          'tailwindcss',
-          'ts_ls',
-          'vue_ls'
+      vim.lsp.enable({
+        'lua_ls',
+        'eslint',
+        'gdscript',
+        'prettier',
+        'tailwindcss',
+        'ts_ls',
+        'vue_ls'
       })
 
-      vim.lsp.config('lua_ls', {})
-      vim.lsp.config('eslint', {})
-      vim.lsp.config('gdscript', {})
-      vim.lsp.config('prettier', {})
-      vim.lsp.config('tailwindcss', {})
-      vim.lsp.config('ts_ls', {})
-      vim.lsp.config('vue_ls', {})
+      vim.lsp.config('lua_ls', {
+        capabilities = capabilities
+      })
+      vim.lsp.config('eslint', {
+        capabilities = capabilities
+
+      })
+      vim.lsp.config('gdscript', {
+        capabilities = capabilities
+      })
+      vim.lsp.config('prettier', {
+        capabilities = capabilities
+      })
+      vim.lsp.config('tailwindcss', {
+        capabilities = capabilities
+      })
+      vim.lsp.config('ts_ls', {
+        capabilities = capabilities
+      })
+      vim.lsp.config('vue_ls', {
+        capabilities = capabilities
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition , {})
       vim.keymap.set({ 'n' }, '<leader>ca' , vim.lsp.buf.code_action, {})
-
 
     end
   },
