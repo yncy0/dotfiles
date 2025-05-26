84d9ac801d7299ae3cb269e1292a5b4eaf238526
Yncy0 <paalayancy@gmail.com>
2025-05-26 14:36:14 +0800

    added: none-ls.lua for formatting

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -5,66 +5,85 @@ return {
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
-    },    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       require("mason").setup({
         ui = {
           icons = {
             package_installed = "✓",
             package_pending = "➜",
-            package_uninstalled = "✗"
-          }
-        }
+            package_uninstalled = "✗",
+          },
+        },
       })
+
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
           "eslint",
           "tailwindcss",
           "ts_ls",
-          "vue_ls"
+          "vue_ls",
         },
-        automatic_installation = true
+        automatic_installation = true,
       })
 
       vim.lsp.enable({
-        'lua_ls',
-        'eslint',
-        'gdscript',
-        'prettier',
-        'tailwindcss',
-        'ts_ls',
-        'vue_ls'
+        "lua_ls",
+        "eslint",
+        "gdscript",
+        "prettier",
+        "tailwindcss",
+        "ts_ls",
+        "vue_ls",
       })
 
-      vim.lsp.config('lua_ls', {
-        capabilities = capabilities
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
       })
-      vim.lsp.config('eslint', {
-        capabilities = capabilities
+      vim.lsp.config("eslint", {
+        capabilities = capabilities,
+      })
+      vim.lsp.config("gdscript", {
+        capabilities = capabilities,
+      })
+      vim.lsp.config("prettier", {
+        capabilities = capabilities,
+      })
+      vim.lsp.config("tailwindcss", {
+        capabilities = capabilities,
+      })
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.config("vue_ls", {
+        capabilities = capabilities,
+      })
 
-      })
-      vim.lsp.config('gdscript', {
-        capabilities = capabilities
-      })
-      vim.lsp.config('prettier', {
-        capabilities = capabilities
-      })
-      vim.lsp.config('tailwindcss', {
-        capabilities = capabilities
-      })
-      vim.lsp.config('ts_ls', {
-        capabilities = capabilities
-      })
-      vim.lsp.config('vue_ls', {
-        capabilities = capabilities
-      })
+      --      vim.api.nvim_create_autocmd("LspAttach", {
+      --        group = vim.api.nvim_create_augroup("my.lsp", {}),
+      --        callback = function(args)
+      --          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      --          if
+      --              not client:supports_method("textDocument/willSaveWaitUntil")
+      --              and client:supports_method("textDocument/formatting")
+      --          then
+      --            vim.api.nvim_create_autocmd("BufWritePre", {
+      --              group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+      --              buffer = args.buf,
+      --              callback = function()
+      --                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+      --              end,
+      --            })
+      --          end
+      --        end,
+      --      })
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition , {})
-      vim.keymap.set({ 'n' }, '<leader>ca' , vim.lsp.buf.code_action, {})
-
-    end
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
   },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
 
     configs.setup({
       auto_install = true,
-      sync_install = false,
       highlight = { enable = true },
       indent = { enable = true }
     })
