c112a25ae94342f250a99afafc6b386c160aa4f4
Yncy0 <paalayancy@gmail.com>
2025-05-28 17:51:46 +0800

    added: mini -> comments, completion, icons

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -1,15 +1,40 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-	},
-	{
-		"echasnovski/mini.files",
-		version = false,
-		config = function()
-			require("mini.files").setup()
+  -- mini.nvim
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+  },
+  -- mini.files
+  {
+    "echasnovski/mini.files",
+    version = false,
+    config = function()
+      require("mini.files").setup()
 
-			vim.keymap.set("n", "<C-n>", ":lua MiniFiles.open()<CR>", {})
-		end,
-	},
+      vim.keymap.set("n", "<C-n>", ":lua MiniFiles.open()<CR>", {})
+    end,
+  },
+  -- mini.icons
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup({
+        style = "glyph",
+      })
+    end,
+  },
+  -- mini.completion
+  {
+    "echasnovski/mini.completion",
+    version = false,
+    config = function()
+      require("mini.completion").setup()
+    end,
+  },
+  --mini.comments
+  {
+    "echasnovski/mini.comment",
+    version = false
+  },
 }
