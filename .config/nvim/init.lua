03be348ad77c6490c701b3096e2e14e9dd21303d
Yncy0 <paalayancy@gmail.com>
2025-06-03 14:51:03 +0800

    update: custome keybinds on tmux.conf and keybinds.lua -> Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+require("config.keymaps")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
