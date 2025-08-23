265739b3056f4f203f3ea51b2cd13439adbb2072
Yncy0 <yncyncy0@gmail.com>
2025-08-23 09:55:08 +0800

    bring back mini-pairs because I don't know why I deleted that

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -4,35 +4,43 @@
 -- @return {}
 
 return {
-	-- mini.nvim
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-	},
-	-- mini.files
-	{
-		"echasnovski/mini.files",
-		version = false,
+  -- mini.nvim
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+  },
+  -- mini.files
+  {
+    "echasnovski/mini.files",
+    version = false,
     enabled = false,
-		config = function()
-			require("mini.files").setup()
+    config = function()
+      require("mini.files").setup()
 
-			vim.keymap.set("n", "<leader>n", ":lua MiniFiles.open()<CR>", {})
-		end,
-	},
-	-- mini.icons
-	{
-		"echasnovski/mini.icons",
-		version = false,
-		config = function()
-			require("mini.icons").setup({
-				style = "glyph",
-			})
-		end,
-	},
-	--mini.comments
-	{
-		"echasnovski/mini.comment",
-		version = false,
-	},
+      vim.keymap.set("n", "<leader>n", ":lua MiniFiles.open()<CR>", {})
+    end,
+  },
+  -- mini.icons
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup({
+        style = "glyph",
+      })
+    end,
+  },
+  --mini.comments
+  {
+    "echasnovski/mini.comment",
+    version = false,
+  },
+  --mini.pairs
+  {
+    "echasnovski/mini.pairs",
+    version = false,
+    config = function ()
+      require("mini.pairs").setup()
+    end
+  },
 }
